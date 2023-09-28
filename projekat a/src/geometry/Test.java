63b178db7c3a27664e9e886b618d6bd4560e515c
Boomerang85 <whiteluke06@gmail.com>
2023-09-28 19:01:42 +0200

    Prvi zadatak (Line.java)

--- projekat a/src/geometry/Test.java
+++ projekat a/src/geometry/Test.java
@@ -4,19 +4,31 @@ public class Test {
 
 	public static void main(String[] args) 
 	{
-		Point point1 = new Point();
+		//Ovo je zadatak sa proslog casa (proveri Point.java)
 		
-		point1.setX(10);
-		point1.setY(5);
+		//Point point1 = new Point();
 		
-		System.out.println("X koordinata tacke je " + point1.getX());
-		System.out.println("Y koordinata tacke je " + point1.getY());
-		System.out.println("Da li je tacka selektovana " + point1.isSelected());
+		//point1.setX(10);
+		//point1.setY(5);
 		
-		double distanceResult = point1.distance(15, 7);
-		System.out.println(distanceResult);
+		//System.out.println("X koordinata tacke je " + point1.getX());
+		//System.out.println("Y koordinata tacke je " + point1.getY());
+		//System.out.println("Da li je tacka selektovana " + point1.isSelected());
 		
-		System.out.println(point1);
+		//double distanceResult = point1.distance(15, 7);
+		//System.out.println(distanceResult);
+		
+		//System.out.println(point1);
+		
+		
+		//Ovo je prvi zadatak (proveri Line.Java)
+		
+		
+		Point point1 = new Point(2, 3);
+		Point point2 = new Point(4, 5);
+		Line line1 = new Line();
+		
+		System.out.println(line1.length(line1.getStartPoint(), line1.getEndPoint()));  
 	}
 
 }
